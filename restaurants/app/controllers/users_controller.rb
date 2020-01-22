71c9aaaee2a9ca1bf8fe94f6acdc9a4cce10fe71
Alina Kornilova <alinakornilova78@gmail.com>
2020-01-22 04:43:50 +0000

    new

--- restaurants/app/controllers/users_controller.rb
+++ restaurants/app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
   end 
   
   post '/users' do
+    # user should not be able to create an account with blank credentials 
+    # user should not be able to create an account with a username that already exists 
+    User.new(params) 
+    if user.username.empty?  || user.password.empty?
+      @error= "Username and password can`t be blank"
+      erb:'users/signup'
+    else 
+      user.save 
+      session[:user_id]= user.id 
+      redirect '/restaurants'
+    end 
   end 
   
   # users can delete account 
