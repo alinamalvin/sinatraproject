44051375d68a05c2c3902821de5baddce4b55230
Alina Kornilova <alinakornilova78@gmail.com>
2020-01-22 05:47:12 +0000

    new

--- restaurants/app/controllers/sessions_controller.rb
+++ restaurants/app/controllers/sessions_controller.rb
@@ -1,8 +1,29 @@
-class SessionsControler < ApplicationController
+class SessionsController < ApplicationController
   
   # users can log in
+  get '/login' do
+   erb :'users/login'
+  end 
   
   # users can log out 
+  post '/login' do
+    if params["username"].empty?  || params["password"].empty?
+      error= "Username and password can`t be blank"
+      erb:'users/login'
+    else if user= User.find_by(username: params["username"], password: params["password"])
+        session[:user_id]= user.id
+        redirect '/restaurants'
+      else 
+        @error = "Account not found"
+        erb :'users/login'
+      end 
+    end 
+  end 
+  
+  get '/logout' do
+    session.clear 
+    redirect '/'
+  end 
   
 end 
   
