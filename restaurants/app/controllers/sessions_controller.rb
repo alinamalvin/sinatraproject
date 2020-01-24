01ba43836a65bf8bddc7dca7dd6c32d908bfca45
Alina Kornilova <alinakornilova78@gmail.com>
2020-01-24 02:02:49 +0000

    new

--- restaurants/app/controllers/sessions_controller.rb
+++ restaurants/app/controllers/sessions_controller.rb
@@ -26,4 +26,5 @@ class SessionsController < ApplicationController
   end 
   
 end 
+
   
--- restaurants/db/schema.rb
+++ restaurants/db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_01_24_001909) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
